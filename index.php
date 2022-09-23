b04b693899404394f2a1432d1ed3b82831651716
carlosavila <carlos.avila@ucsm.edu.pe>
2022-09-22 19:03:51 -0500

    terminado y modificado con css

--- index.php
+++ index.php
@@ -9,40 +9,39 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Inicio de Sesion</title>
-        <link rel="stylesheet" href="style.css">
+        <title>Registro de Clientes</title>
+        <link rel="stylesheet" type="text/css" href="style.css?ts=<?=time()?>" />
         <meta name=""viewport content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
-        <title>REGISTRO DE CLIENTES</title>
     </head>
 <div>
     <body>
+    <div class="users-form">
         <form action="register.php" method="POST">
-                <h1>REGISTRO DE CLIENTES</h1>
+            <br>
+                <h1>Ingresar datos del Cliente</h1>
                 <input type="text" name="dni" placeholder="DNI" required>
-                <br>
                 <input type="text" name="name" placeholder="NOMBRE" required>
-                <br>
                 <input type="text" name="phone_number" placeholder="NUMERO TELEFONICO" required>
-                <br>
-                <input type="text" name="address" placeholder="DIRECCION" required>
-                <br>
-                <input type="text" name="email" placeholder="EMAIL" required>
-                <br>
+                <input type="text" name="address" placeholder="DIRECCION" required>              
+                <input type="text" name="email" placeholder="EMAIL" required>           
                 <input type="submit" value="Agregar">
         </form>
+    </div>
     </body>
 </div>
 <div class="users-table">
+    <br>
     <h2>Usuarios registrados</h2>
+    <br>
     <table>
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Dni</th>
-                <th>Nombre</th>
-                <th>Numero Telefonico</th>
-                <th>Direccion</th>
-                <th>Email</th>
+                <th>DNI</th>
+                <th>NOMBRE</th>
+                <th>NUMERO DE TELEFONO</th>
+                <th>DIRECCION</th>
+                <th>EMAIL</th>
                 <th></th>
                 <th></th>
             </tr>
@@ -50,18 +49,20 @@
         <tbody>
             <?php while ($row = mysqli_fetch_array($query)): ?>
                 <tr>
-                    <th><?= $row['id'] ?></th>
-                    <th><?= $row['dni'] ?></th>
-                    <th><?= $row['name'] ?></th>
-                    <th><?= $row['phone_number'] ?></th>
-                    <th><?= $row['address'] ?></th>
-                    <th><?= $row['email'] ?></th>
-                    <th><a href="update.php?id=<?= $row['id'] ?>" class="users-table--edit">Editar</a></th>
-                    <th><a href="delete.php?id=<?= $row['id'] ?>" class="users-table--delete">Eliminar</a></th>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['dni'] ?></td>
+                    <td><?= $row['name'] ?></td>
+                    <td><?= $row['phone_number'] ?></td>
+                    <td><?= $row['address'] ?></td>
+                    <td><?= $row['email'] ?></td>
+                    <td><a href="update.php?id=<?= $row['id'] ?>" class="users-table--edit">Editar</a></td>
+                    <td><a href="delete.php?id=<?= $row['id'] ?>" class="users-table--delete">Eliminar</a></td>
                 </tr>
             <?php endwhile; ?>
         </tbody>
     </table>
+    <br>
+    <br>
 </div>
 
 </html>
--- update.php
+++ update.php
@@ -13,19 +13,24 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="style.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="style.css?ts=<?=time()?>" />
         <title>Editar Cliente</title>
         
     </head>
     <body>
         <div class="users-form">
             <form action="edit.php" method="POST">
-                <h1>EDITAR CLIENTE</h1>
+                <h1>Editar datos del Cliente</h1>
                 <input type="hidden" name="id" value="<?= $row['id']?>">
+                <p>Actualizar DNI</p>
                 <input type="text" name="dni" placeholder="Dni" value="<?= $row['dni']?>">
+                <p>Actualizar Nombre</p>
                 <input type="text" name="name" placeholder="Nombre" value="<?= $row['name']?>">
+                <p>Actualizar Numero Telefonico</p>
                 <input type="text" name="phone_number" placeholder="Numero Telefonico" value="<?= $row['phone_number']?>">
+                <p>Actualizar Direccion</p>
                 <input type="text" name="address" placeholder="Direccion" value="<?= $row['address']?>">
+                <p>Actualizar Email</p>
                 <input type="text" name="email" placeholder="Email" value="<?= $row['email']?>">
 
                 <input type="submit" value="Actualizar">
